7375ead3b8bc3b11a0750a679a752b8c935bcff3
Ivan <ivan.polyakov.2001@mail.ru>
2021-04-06 17:08:37 +0300

    Две кнопки и 2 страницы

---
+++ app/src/main/java/com/example/DotaHelper/MainActivity.java
@@ -0,0 +1,51 @@
+package com.example.DotaHelper;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        addListenerOnButtonHeroesMenu ();
+        addListenerOnButtonItemsMenu ();
+
+    }
+
+    private Button btnHeroesMenu;
+    private Button btnItemsMenu;
+
+    public void addListenerOnButtonHeroesMenu () {
+        btnHeroesMenu = (Button)findViewById(R.id.btnHeroesMenu);
+
+        btnHeroesMenu.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Intent intent = new Intent(".Heroes");
+                        startActivity(intent);
+                    }
+                }
+        );
+    }
+
+    public void addListenerOnButtonItemsMenu () {
+        btnItemsMenu = (Button)findViewById(R.id.btnItemsMenu);
+
+        btnItemsMenu.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Intent intent = new Intent(".Items");
+                        startActivity(intent);
+                    }
+                }
+        );
+    }
+}
