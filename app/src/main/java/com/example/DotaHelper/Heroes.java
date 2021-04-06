b4e2f79aa5ae8630a7102cb0d545b246ec62b729
Ivan <ivan.polyakov.2001@mail.ru>
2021-04-06 23:20:01 +0300

    Корректировка интерфейса (хочу спать)

--- app/src/main/java/com/example/DotaHelper/Heroes.java
+++ app/src/main/java/com/example/DotaHelper/Heroes.java
@@ -3,6 +3,8 @@ package com.example.DotaHelper;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Heroes extends AppCompatActivity {
 
@@ -10,5 +12,36 @@ public class Heroes extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_heroes);
+        addListenerOnButtonSortAlf ();
+        addListenerOnButtonSortAtr ();
+    }
+
+    private Button btnHeroesSortAlf;
+    private Button btnHeroesSortAtr;
+
+    public void addListenerOnButtonSortAlf () {
+        btnHeroesSortAlf = (Button)findViewById(R.id.btnItemsSortAlf);
+
+        btnHeroesSortAlf.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+
+                    }
+                }
+        );
+    }
+
+    public void addListenerOnButtonSortAtr () {
+        btnHeroesSortAtr = (Button)findViewById(R.id.btnItemssSortAtr);
+
+        btnHeroesSortAtr.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+
+                    }
+                }
+        );
     }
 }
--- app/src/main/java/com/example/DotaHelper/Items.java
+++ app/src/main/java/com/example/DotaHelper/Items.java
@@ -11,4 +11,6 @@ public class Items extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_items);
     }
+
+
 }
